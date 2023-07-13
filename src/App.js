3ff8a243bef5c6f72ac1018562da3611002708bb
vishal <vk7821188@gmail.com>
2023-07-13 12:10:48 +0530

    small_change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Nav from './components/Nav';
 function App() {
   return (
     <div className="App">
-      <h1 className='banner'>Content Management site</h1>
+      <h1 className='banner'>Content Listing site</h1>
       <BrowserRouter>
         <Nav/>
       </BrowserRouter>
